eb92229ed93116253b6ce00cf6be8cad56c72c5f
Moonipur <119776865+Moonipur@users.noreply.github.com>
2023-04-13 18:27:35 +0700

    Add files via upload

--- src/Organized_file.py
+++ src/Organized_file.py
@@ -1,18 +1,59 @@
 from __init__ import *
 
-def heat(file_):
-    pass
+def heat(file_, labels):
+    df = pd.read_csv(f"{file_}")
+    df.columns = df.columns.str.strip().str.upper()
+    symbol = []
+    ex = []
+    label_g = [x.upper() for x in labels]
+
+    for col in df.columns:
+        if col == 'SYMBOL':
+            symbol.append('SYMBOL')
+        elif col == 'GENE':
+            symbol.append('GENE')
+        elif col == 'PROTEIN':
+            symbol.append('PROTEIN')
+        elif col == 'NAME':
+            symbol.append('NAME')
+        else:
+            ex.append(col)
+    
+    expression = list(set(ex).difference(label_g))
+    
+    if len(symbol) != 1:
+        print("**Error: Your data has NOT SYMBOL or PROTEIN or GENE column")
+        exit()
+
+    if len(expression) != 1:
+        print("**Error: Your data has NOT EXPRESSION column")
+        exit()
+
+    new_df = pd.DataFrame()
+    new_df['SYMBOL'] = df[f'{symbol[0]}']
+    new_df['EXPRESSION'] = df[f'{expression[0]}']
+    for i in label_g:
+        new_df[i] = df[i]
+
+    new_df.to_csv(f"{file_[:-4]}_heatm.csv", index=False)
+    print(">> Data for heatmap <<")
+    print(new_df)
+    print("-------------------------------------------------------------------------------------------------------------------------------------")
+    print(">> Finished organized and Converted to DataFrame <<")
+    print("-------------------------------------------------------------------------------------------------------------------------------------")
+    return(f"{file_[:-4]}_heatm.csv")
 
 def venn(file_):
     pass
 
-def volc(file_, p_type):
+def volc(file_, p_type, inter):
     df = pd.read_csv(f"{file_}")
     df.columns = df.columns.str.strip().str.upper()
     symbol = []
     log2fc = []
     p_val = []
     log_p_val = []
+    interest = []
 
     for col in df.columns:
         if col == 'LOG2':
@@ -49,6 +90,16 @@ def volc(file_, p_type):
             log_p_val.append('LOG10')
         elif col == 'LOG10P':
             log_p_val.append('LOG10P')
+        elif col == 'INTEREST':
+            interest.append('INTEREST')
+        elif col == 'INTERESTGENE':
+            interest.append('INTERESTGENE')
+        elif col == 'INTEREST_GENE':
+            interest.append('INTEREST_GENE')
+        elif col == 'INTERESTPROTEIN':
+            interest.append('INTERESTPROTEIN')
+        elif col == 'INTEREST_PROTEIN':
+            interest.append('INTEREST_PROTEIN')
         else:
             pass
     
@@ -68,10 +119,15 @@ def volc(file_, p_type):
         if len(log2fc) != 1:
             print("**Error: Your data has NOT LogP_value column")
             exit()
-    
+
     new_df = pd.DataFrame()
     new_df['SYMBOL'] = df[f'{symbol[0]}']
     new_df['Log2FC'] = df[f'{log2fc[0]}']
+
+    if inter == 'yes':
+        new_df['INTEREST'] = df[f'{interest[0]}']
+    elif inter == 'no':
+        pass
 
     if p_type == 'P_value':
         new_df['P_value'] = df[f'{p_val[0]}']
@@ -109,24 +165,34 @@ def copy(file_):
 def splr(file_):
     pass
 
-def Organize_file(file_csv, type_, p):
+def corr(file_):
+    pass
+
+def Organize_file(file_csv, type_, p='', inter='', label=[]):
     if type_ == 'heatmap':
-        heat(file_csv)
+        df = heat(file_csv, labels=label)
+        return(df)
     elif type_ == 'vennDiagram':
         venn(file_csv)
+        return(df)
     elif type_ == 'volcano':
-        df = volc(file_csv, p_type = p)
+        df = volc(file_csv, p_type = p, inter=inter)
         return(df)
     elif type_ == 'distribution':
-        dist(file_csv)
+        df = dist(file_csv)
+        return(df)
     elif type_ == 'bubble':
-        bubb(file_csv)
+        df = bubb(file_csv)
+        return(df)
     elif type_ == 'boxplot':
-        box(file_csv)
+        df = box(file_csv)
+        return(df)
     elif type_ == 'copyNumber':
-        copy(file_csv)
+        df = copy(file_csv)
+        return(df)
     elif type_ == 'splRatio':
-        splr(file_csv)
-    else:
-        print(f"**Error: The type '{type_}' is Unrecognized!")
-        exit()
+        df = splr(file_csv)
+        return(df)
+    elif type_ == 'correlation':
+        df = corr(file_csv)
+        return(df)
--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ import os, sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import argparse
 import math
 
