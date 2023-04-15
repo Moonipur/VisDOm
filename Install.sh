a234e44a63e552bab26915128937936229c9d733
Moonipur <119776865+Moonipur@users.noreply.github.com>
2023-04-15 15:12:05 +0700

    Add files via upload

---
+++ Install.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+
+module=(Bubble CopyNumber Correlation Distribution Heatmap SpL_ratio VennDiagram Volcano)
+
+if [ -f ~/.bashrc ];
+then
+    for i in ${module[@]}; 
+    do
+       Loc=`find "$(pwd)" -name "${i}.sh"`;
+       echo "alias ${i}='python ${Loc}'" >> ~/.bashrc;
+    done
+   echo "Installation is already finished";
+else
+   echo "Your .bashrc does NOT exist";
+fi
